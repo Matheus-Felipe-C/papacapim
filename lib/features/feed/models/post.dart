7eec5a517a3bebd64df1333178b5d5cb5bd51161
Luiz Guilherme <108948761+gui-andrad@users.noreply.github.com>
2025-02-25 09:10:22 -0300

    Update: adicionando botão de curtir post.dart

--- lib/features/feed/models/post.dart
+++ lib/features/feed/models/post.dart
@@ -1,8 +1,15 @@
 import 'package:papacapim/features/auth/models/user.dart';
 
 class Post {
-  final String message;
-  final User user;
+  final String message; // Conteúdo do post
+  final User user; // Usuário que criou o post
+  int likes; // Número de curtidas
+  bool isLikedByCurrentUser; // Indica se o usuário atual curtiu o post
 
-  Post({required this.message, required this.user});
+  Post({
+    required this.message,
+    required this.user,
+    this.likes = 0, // Valor padrão: 0 curtidas
+    this.isLikedByCurrentUser = false, // Valor padrão: não curtido
+  });
 }
