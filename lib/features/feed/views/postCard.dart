3a03146035560db25c115579dbaa6d4e64496e84
Luiz Guilherme <108948761+gui-andrad@users.noreply.github.com>
2025-02-25 09:16:24 -0300

    Update: adicionando botão de curtir postCard.dart

--- lib/features/feed/views/postCard.dart
+++ lib/features/feed/views/postCard.dart
@@ -4,27 +4,55 @@ import 'package:papacapim/styles.dart';
 class PostCard extends StatelessWidget {
   final String message;
   final String username;
+  final int likes;
+  final bool isLikedByCurrentUser;
+  final VoidCallback onLikePressed;
 
-  const PostCard({super.key, required this.message, required this.username});
+  const PostCard({
+    super.key,
+    required this.message,
+    required this.username,
+    required this.likes,
+    required this.isLikedByCurrentUser,
+    required this.onLikePressed,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      margin: const EdgeInsets.all(8.0),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("@$username",
-                style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.grey)),
+            // Nome do usuário
             Text(
-              message,
-              style: AppStyles.bodyText,
-            )
+              username,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 8),
+
+            // Mensagem do post
+            Text(message),
+            const SizedBox(height: 8),
+
+            // Botão de curtir e número de curtidas
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(
+                    isLikedByCurrentUser ? Icons.favorite : Icons.favorite_border,
+                    color: isLikedByCurrentUser ? Colors.red : null,
+                  ),
+                  onPressed: onLikePressed,
+                ),
+                Text('$likes curtidas'),
+              ],
+            ),
           ],
         ),
       ),
