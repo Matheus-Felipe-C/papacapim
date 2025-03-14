b967ccfee484c82bdf3765abb3b1eb70b5e60a11
Matheus Felipe <theusf40@hotmail.com>
2025-03-14 07:56:35 -0300

    Textarea de criar posts agora expande ao tamanho disponível da tela

--- lib/features/feed/views/newPostScreen.dart
+++ lib/features/feed/views/newPostScreen.dart
@@ -2,45 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:papacapim/styles.dart';
 
 class NewPostScreen extends StatefulWidget {
-    const NewPostScreen({super.key});
+  const NewPostScreen({super.key});
 
   @override
   State<NewPostScreen> createState() => _NewPostScreenState();
 }
 
 class _NewPostScreenState extends State<NewPostScreen> {
-    final TextEditingController _contentController = TextEditingController();
-    
-    void _submitPost(BuildContext context){
-        if (_contentController.text.isEmpty) return;
+  final TextEditingController _contentController = TextEditingController();
 
-        Navigator.pop(context, {"message": _contentController.text});
-    }
+  void _submitPost(BuildContext context) {
+    if (_contentController.text.isEmpty) return;
 
-    @override
+    Navigator.pop(context, {"message": _contentController.text});
+  }
+
+  @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text("New post"),
-      actions: <Widget>[
-        ElevatedButton(
-          onPressed: () => _submitPost(context),
-          child: const Text("Postar"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("New post"),
+        actions: <Widget>[
+          ElevatedButton(
+            onPressed: () => _submitPost(context),
+            child: const Text("Postar"),
           )
-      ],
-      ),
-    body: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: _contentController,
-            decoration: AppStyles.textFieldDecoration("Escreva sua mensagem aqui"),
-            maxLines: 4,
-          ),
         ],
       ),
-    ),
-  );
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.all(8),
+        child: TextField(
+          expands: true,
+          maxLines: null,
+          keyboardType: TextInputType.multiline,
+          textAlignVertical: TextAlignVertical.top,
+          controller: _contentController,
+          decoration:
+              AppStyles.textFieldDecoration("Escreva sua mensagem aqui"),
+        ),
+      ),
+    );
   }
 }
