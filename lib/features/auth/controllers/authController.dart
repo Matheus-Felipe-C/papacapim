02c0e47b5be71cb45351416978f76f0482f1fa51
Matheus Felipe <theusf40@hotmail.com>
2025-04-09 22:58:18 -0300

    Modificado authController para chamar as funções diretamente do ApiService

--- lib/features/auth/controllers/authController.dart
+++ lib/features/auth/controllers/authController.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:papacapim/api_service.dart';
+import 'package:papacapim/features/auth/models/session.dart';
 import 'package:papacapim/features/auth/models/user.dart';
 
 class AuthController extends ChangeNotifier{
-  final ApiService _apiService = ApiService();
   List<User> users = [];
 
   Future<bool> createUser(String name, String username, String password, String passwordConfirm) async {
     try {
-      User newUser = await _apiService.createUser(username, name, password, passwordConfirm);
-      users.add(newUser);
-      notifyListeners();
-      print(newUser);
+      User newUser = await ApiService().createUser(username, name, password, passwordConfirm);
+      Session newSession = await ApiService().createSession(username, password);
       return true;
     } catch (e) {
       print("Erro ao criar usuário: $e");
       return false;
     }
   }
-}
+  
