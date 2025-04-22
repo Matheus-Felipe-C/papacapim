213eb2cbb6b1b89d9aa5d472924a8941cb25e63b
Matheus Felipe <theusf40@hotmail.com>
2025-04-21 22:20:38 -0300

    Criado função para atualizar o usuário no authController

--- lib/features/auth/controllers/authController.dart
+++ lib/features/auth/controllers/authController.dart
@@ -8,18 +8,17 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController extends ChangeNotifier {
   Session? _session;
+  String? _userPass;
 
   Session? get session => _session;
   String? get token => _session?.token;
+  String? get userPass => _userPass;
 
-  //Carrega a sessão inicial
-  Future<void> loadSession() async {
-    final prefs = await SharedPreferences.getInstance();
-    final jsonStr = prefs.getString('session');
-    if (jsonStr != null) {
-      _session = Session.fromJson(jsonDecode(jsonStr));
-      notifyListeners();
-    }
+  // Cria um novo usuário
+  Future<void> login(String login, String password) async {
+    final session = await ApiService().createSession(login, password);
+    await saveSessionToPrefs(session);
+    _userPass = password;
   }
 
   /// Cria um novo usuário e sessão a partir da API
@@ -35,12 +34,48 @@ class AuthController extends ChangeNotifier {
     return true;
   }
 
+  Future<void> updateUser(
+    String userId,
+    String login,
+    String? name,
+    String? password,
+    String? passConfirm,
+  ) async {
+    User updatedUser = await ApiService().updateUser(
+      token!,
+      userId,
+      login: login,
+      name: name,
+      password: password,
+      passwordConfirmation: passConfirm,
+    );
+
+    // Determina qual senha utilizar para re-autenticação
+    late String usedPassword;
+
+    if (password != null && passConfirm != null) {
+      if (password != passConfirm) {
+        throw Exception('As senhas não coincidem.');
+      }
+      usedPassword = password;
+      _userPass = password;
+    } else {
+      if (_userPass == null) {
+        throw Exception('Senha atual desconhecida. Faça login novamente.');
+      }
+      usedPassword = _userPass!;
+    }
+
+    Session newSession = await ApiService().createSession(login, usedPassword);
+    _session = newSession;
+    notifyListeners();
+  }
+
   /// Salva a sessão
   Future<void> saveSessionToPrefs(Session session) async {
     final prefs = await SharedPreferences.getInstance();
     String userJson = jsonEncode(session.toJson());
     await prefs.setString('session', userJson);
     _session = session;
-    notifyListeners();
   }
 }
