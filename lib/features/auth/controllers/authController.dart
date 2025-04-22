650f12ee40038262876e89e2a3247d43a6fc0b6c
Matheus Felipe <theusf40@hotmail.com>
2025-04-21 21:53:24 -0300

    Criado função para carregar a nova sessão

--- lib/features/auth/controllers/authController.dart
+++ lib/features/auth/controllers/authController.dart
@@ -6,25 +6,33 @@ import 'package:papacapim/features/auth/models/session.dart';
 import 'package:papacapim/features/auth/models/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class AuthController extends ChangeNotifier{
+class AuthController extends ChangeNotifier {
   Session? _session;
 
   Session? get session => _session;
   String? get token => _session?.token;
 
-  /// Cria um novo usuário e sessão a partir da API
-  Future<bool> createUser(String name, String username, String password, String passwordConfirm) async {
-    try {
-      User newUser = await ApiService().createUser(username, name, password, password);
-      Session newSession = await ApiService().createSession(username, password);
-      print(newUser);
-      print(newSession);
-      await saveSessionToPrefs(newSession);
-      return true;
-    } catch (e) {
-      print("Erro ao criar usuário: $e");
-      return false;
+  //Carrega a sessão inicial
+  Future<void> loadSession() async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonStr = prefs.getString('session');
+    if (jsonStr != null) {
+      _session = Session.fromJson(jsonDecode(jsonStr));
+      notifyListeners();
     }
+  }
+
+  /// Cria um novo usuário e sessão a partir da API
+  Future<bool> createUser(String name, String username, String password,
+      String passwordConfirm) async {
+    User newUser =
+        await ApiService().createUser(username, name, password, password);
+    Session newSession = await ApiService().createSession(username, password);
+    print(newUser);
+    print(newSession);
+    await saveSessionToPrefs(newSession);
+    notifyListeners();
+    return true;
   }
 
   /// Salva a sessão
@@ -32,6 +40,7 @@ class AuthController extends ChangeNotifier{
     final prefs = await SharedPreferences.getInstance();
     String userJson = jsonEncode(session.toJson());
     await prefs.setString('session', userJson);
+    _session = session;
+    notifyListeners();
   }
 }
-  
